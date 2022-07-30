c02dfc524104ab6fd1d6d79832dd8fe64cbc8b51
SvitlanaTymchii <109731357+SvitlanaTymchii@users.noreply.github.com>
2022-07-30 11:41:19 +0100

    Start from NewYork + SearchEngineByHands

--- src/app.js
+++ src/app.js
@@ -39,45 +39,51 @@ ${currentHours}:${currentMin}  `;
 
 // current temperature
 
-function showTemperatureLocation(response) {
+function showAll(response) {
   //функція виводить температуру по поточній локації (координати чи назва міста)
-  let currentTemperature = document.querySelector("#current-temperature");
-  let currentLocation = document.querySelector("#current-location");
-  let descriptionElement = document.querySelector("#description");
-  let humidity = document.querySelector("#humidity");
-  let speed = document.querySelector("#speed");
-  let iconElement = document.querySelector("#icon");
+  let currentTemperature = document.querySelector("#current-temperature"); //знаходим на сторінці елемент  для виведення температури
+  let currentLocation = document.querySelector("#current-location"); // елемент для виведення поточноі локаціі
+  let descriptionElement = document.querySelector("#description"); // елемент для опису погоди
+  let humidity = document.querySelector("#humidity"); //елемент вологість
+  let speed = document.querySelector("#speed"); // елемент куди виводиться швидкість вітру
+  let iconElement = document.querySelector("#icon"); // картинка погоди
 
-  currentTemperature.innerHTML = `${Math.round(response.data.main.temp)}° `;
-  currentLocation.innerHTML = `${response.data.name}`;
-  descriptionElement.innerHTML = `${response.data.weather[0].description}`;
-  humidity.innerHTML = `${response.data.main.humidity}%`;
-  speed.innerHTML = `${response.data.wind.speed}km/h`;
+  currentTemperature.innerHTML = `${Math.round(response.data.main.temp)}° `; // підхоплюємо з АПІ температуру і виводимо
+  currentLocation.innerHTML = `${response.data.name}`; // підтягуємо з бази назву міста
+  descriptionElement.innerHTML = `${response.data.weather[0].description}`; // витягуємо опис погоди
+  humidity.innerHTML = `${response.data.main.humidity}%`; // витягуємо і друкуємо про вологість
+  speed.innerHTML = `${response.data.wind.speed}km/h`; // і про швидкість вітру
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` // міняємо атрибут посилання картинки на посилання на іконки в апі
   );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
+  iconElement.setAttribute("alt", ` response.data.weather[0].description`); // і те саме робимо з описом
+  //????? Чи коректно тут працюэ альт????
 }
 
 // пошук по натисканню кнопки Search
-function city(position) {
+/*function cityPos(position) {
   let cityName = document.querySelector("#cityname-input");
   let city = cityName.value;
-  // console.log(`${city}`);
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(`${apiUrl}`).then(showTemperatureLocation);
-}
+  axios.get(`${apiUrl}`).then(showAll);
+}*/
 
 //form
-function stopClear(event) {
+function search(event) {
   event.preventDefault(); //вже не перезавантажується сторінка
-  navigator.geolocation.getCurrentPosition(city);
+  let cityName = document.querySelector("#cityname-input");
+  let city = cityName.value;
+  start(city);
 }
 
-let apiKey = "92dec7e2931d37f76f7ea0cca649963a";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(`${apiUrl}`).then(showTemperatureLocation);
+function start(city) {
+  let apiKey = "92dec7e2931d37f76f7ea0cca649963a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(`${apiUrl}`).then(showAll); // хватаєм апі-юрл і витягуємо функцією showAll все, щ можемо
+}
+
+start("New York");
 
 let form = document.querySelector("#search-form");
-form.addEventListener("submit", stopClear); // шукаємо сабміт форми
+form.addEventListener("submit", search); // шукаємо сабміт форми
